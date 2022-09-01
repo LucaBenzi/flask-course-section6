a4dfc889bc230eed1a34474e99786570f3a01f71
Luca Benzi <benz14@hotmail.it>
2022-09-01 16:21:34 +0200

    using another env var for db

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URLPOSTGRESQL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
